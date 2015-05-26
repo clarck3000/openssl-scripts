e6d8642e8bccd83aa6cca83d93c7e7620b89f178
clark <clark3000@gmail.com>
2015-05-26 11:26:26 +0800

    Use BSD style license

--- gensrv.sh
+++ gensrv.sh
@@ -1,11 +1,9 @@
 MYDIR=$PWD
 export OPENSSL_CONF=$MYDIR/openssl.cnf
 #echo $MYDIR
-mkdir -p pki/nginx
-mkdir -p pki/nginx/private
-cd pki/nginx 
-openssl genrsa -out private/nginx.key 2048
-openssl req -new -key private/nginx.key -out nginx.csr
-openssl x509 -req -in nginx.csr -CA $MYDIR/pki/CA/ca.crt -CAkey $MYDIR/pki/CA/private/ca.pem -CAcreateserial -out nginx.crt
-sudo cp nginx.crt /etc/nginx/
-sudo cp private/nginx.key /etc/nginx/
+mkdir -p pki/server
+mkdir -p pki/server/private
+cd pki/server 
+openssl genrsa -out private/server.key 2048
+openssl req -new -key private/server.key -out server.csr
+openssl x509 -req -in server.csr -CA $MYDIR/pki/CA/ca.crt -CAkey $MYDIR/pki/CA/private/ca.pem -CAcreateserial -out server.crt
